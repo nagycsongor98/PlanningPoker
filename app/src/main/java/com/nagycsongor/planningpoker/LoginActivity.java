43aeb33cb6d97b531180bfbdc664220b06e2c4eb
Szili <nagyszili14@gmail.com>
2019-10-23 20:09:25 +0300

    Login Activity and insert User

---
+++ app/src/main/java/com/nagycsongor/planningpoker/LoginActivity.java
@@ -0,0 +1,44 @@
+package com.nagycsongor.planningpoker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    private EditText name;
+    private Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        name = findViewById(R.id.loginName);
+        loginButton = findViewById(R.id.loginButton);
+
+    }
+
+
+    public void login(View view)
+    {
+        String userName = name.getText().toString();
+
+        //INSERT userName into DB
+        MyDatabase database =new MyDatabase(this);
+        //database.insertProblem("bajvanhey");
+        //database.getP();
+        database.insertUser(userName);
+        String newVoteName = database.getNewVote();
+        database.votedTo(newVoteName);
+
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+
+    }
+}
